0bed9c358607cad9b19c1f46873ae3e441fc564c
milton-salgado <miltonsalgadoleandro@gmail.com>
2025-04-05 01:50:50 -0300

    update: Atualização da linguagem da aplicação

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 from flask import Flask, request, jsonify, render_template
 import sqlite3
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 def init_db():
     with sqlite3.connect('database.db') as conn:
-        conn.execute('CREATE TABLE IF NOT EXISTS livros ('
+        conn.execute('CREATE TABLE IF NOT EXISTS books ('
                      'id INTEGER PRIMARY KEY AUTOINCREMENT, '
-                     'titulo TEXT NOT NULL, '
-                     'categoria TEXT NOT NULL, '
-                     'autor TEXT NOT NULL, '
-                     'imagem_url TEXT NOT NULL)')
-        print('Tabela LIVROS criada com sucesso.')
+                     'title TEXT NOT NULL, '
+                     'category TEXT NOT NULL, '
+                     'author TEXT NOT NULL, '
+                     'image_url TEXT NOT NULL)')
+        print('BOOKS table created successfully.')
 
 
 init_db()
@@ -23,50 +25,50 @@ def homepage():
     return render_template('index.html')
 
 
-@app.route('/doar', methods=['POST'])
-def doar_livro():
-    dados = request.get_json()
-    titulo = dados.get('titulo')
-    categoria = dados.get('categoria')
-    autor = dados.get('autor')
-    imagem_url = dados.get('imagem_url')
+@app.route('/donate', methods=['POST'])
+def donate_book():
+    data = request.get_json()
+    title = data.get('title')
+    category = data.get('category')
+    author = data.get('author')
+    image_url = data.get('image_url')
 
-    if not all([titulo, categoria, autor, imagem_url]):
-        return jsonify({'Erro': 'Todos os campos são obrigatórios'}), 400
+    if not all([title, category, author, image_url]):
+        return jsonify({'Error': 'All fields are required'}), 400
 
     with sqlite3.connect('database.db') as conn:
         cursor = conn.cursor()
-        cursor.execute('INSERT INTO livros (titulo, categoria, autor, imagem_url) VALUES (?, ?, ?, ?)',
-                       (titulo, categoria, autor, imagem_url))
+        cursor.execute('INSERT INTO books (title, category, author, image_url) VALUES (?, ?, ?, ?)',
+                       (title, category, author, image_url))
         conn.commit()
 
-    return jsonify({'Mensagem': 'Livro cadastrado com sucesso!'}), 201
+    return jsonify({'Message': 'Book successfully registered!'}), 201
 
 
-@app.route('/livros', methods=['GET'])
-def listar_livros():
+@app.route('/donated-books', methods=['GET'])
+def list_books():
     with sqlite3.connect('database.db') as conn:
         cursor = conn.cursor()
-        cursor.execute('SELECT * FROM livros')
-        livros = cursor.fetchall()
+        cursor.execute('SELECT * FROM books')
+        books = cursor.fetchall()
 
-    livros_json = [{'id': livro[0], 'titulo': livro[1], 'categoria': livro[2],
-                    'autor': livro[3], 'imagem_url': livro[4]} for livro in livros]
+    books_json = [{'id': book[0], 'title': book[1], 'category': book[2],
+                   'author': book[3], 'image_url': book[4]} for book in books]
 
-    return jsonify({'livros': livros_json}), 200
+    return jsonify({'books': books_json}), 200
 
 
-@app.route('/livros/<int:id>', methods=['DELETE'])
-def deletar_livro(id):
+@app.route('/books/<int:id>', methods=['DELETE'])
+def delete_book(id):
     with sqlite3.connect('database.db') as conn:
         cursor = conn.cursor()
-        cursor.execute('DELETE FROM livros WHERE id = ?', (id,))
+        cursor.execute('DELETE FROM books WHERE id = ?', (id,))
         conn.commit()
 
     if cursor.rowcount == 0:
-        return jsonify({'Erro': 'Livro não encontrado'}), 404
+        return jsonify({'Error': 'Book not found'}), 404
 
-    return jsonify({'Mensagem': 'Livro deletado com sucesso!'}), 200
+    return jsonify({'Message': 'Book successfully deleted!'}), 200
 
 
 if __name__ == "__main__":
